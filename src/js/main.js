9562f5ed8f84b22f6c3df2e32750a95932356a68
tonyle01 <sang.ld@uptivistic.com>
2023-03-23 20:42:58 +0700

    edit url

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,9 @@
 // Add lib
-const web3 = new Web3("https://arb-mainnet.g.alchemy.com/v2/2e-x6lNSeJ-M8DDJmNzL01FKrdh3LmCT");
+// const web3 = new Web3("https://arb-mainnet.g.alchemy.com/v2/2e-x6lNSeJ-M8DDJmNzL01FKrdh3LmCT");
+const web3 = new Web3("https://arbitrum-one.public.blastapi.io");
+// const web3 = new Web3("https://endpoints.omniatech.io/v1/arbitrum/one/public");
+
+// const web3 = new Web3("https://arb-mainnet.g.alchemy.com/v2/fTgP_f0U2lmd4_XSs1iQT56SojMa91wZ");
 const $ = document.querySelector.bind(document);
 const $$ = document.querySelectorAll.bind(document);
 const contract = new web3.eth.Contract([{ "inputs": [{ "internalType": "contract IERC20VotesUpgradeable", "name": "_token", "type": "address" }, { "internalType": "address payable", "name": "_sweepReceiver", "type": "address" }, { "internalType": "address", "name": "_owner", "type": "address" }, { "internalType": "uint256", "name": "_claimPeriodStart", "type": "uint256" }, { "internalType": "uint256", "name": "_claimPeriodEnd", "type": "uint256" }, { "internalType": "address", "name": "delegateTo", "type": "address" }], "stateMutability": "nonpayable", "type": "constructor" }, { "anonymous": false, "inputs": [{ "indexed": true, "internalType": "address", "name": "recipient", "type": "address" }, { "indexed": false, "internalType": "uint256", "name": "amount", "type": "uint256" }], "name": "CanClaim", "type": "event" }, { "anonymous": false, "inputs": [{ "indexed": true, "internalType": "address", "name": "recipient", "type": "address" }, { "indexed": false, "internalType": "uint256", "name": "amount", "type": "uint256" }], "name": "HasClaimed", "type": "event" }, { "anonymous": false, "inputs": [{ "indexed": true, "internalType": "address", "name": "previousOwner", "type": "address" }, { "indexed": true, "internalType": "address", "name": "newOwner", "type": "address" }], "name": "OwnershipTransferred", "type": "event" }, { "anonymous": false, "inputs": [{ "indexed": true, "internalType": "address", "name": "newSweepReceiver", "type": "address" }], "name": "SweepReceiverSet", "type": "event" }, { "anonymous": false, "inputs": [{ "indexed": false, "internalType": "uint256", "name": "amount", "type": "uint256" }], "name": "Swept", "type": "event" }, { "anonymous": false, "inputs": [{ "indexed": true, "internalType": "address", "name": "recipient", "type": "address" }, { "indexed": false, "internalType": "uint256", "name": "amount", "type": "uint256" }], "name": "Withdrawal", "type": "event" }, { "inputs": [], "name": "claim", "outputs": [], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [{ "internalType": "address", "name": "delegatee", "type": "address" }, { "internalType": "uint256", "name": "expiry", "type": "uint256" }, { "internalType": "uint8", "name": "v", "type": "uint8" }, { "internalType": "bytes32", "name": "r", "type": "bytes32" }, { "internalType": "bytes32", "name": "s", "type": "bytes32" }], "name": "claimAndDelegate", "outputs": [], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [], "name": "claimPeriodEnd", "outputs": [{ "internalType": "uint256", "name": "", "type": "uint256" }], "stateMutability": "view", "type": "function" }, { "inputs": [], "name": "claimPeriodStart", "outputs": [{ "internalType": "uint256", "name": "", "type": "uint256" }], "stateMutability": "view", "type": "function" }, { "inputs": [{ "internalType": "address", "name": "", "type": "address" }], "name": "claimableTokens", "outputs": [{ "internalType": "uint256", "name": "", "type": "uint256" }], "stateMutability": "view", "type": "function" }, { "inputs": [], "name": "owner", "outputs": [{ "internalType": "address", "name": "", "type": "address" }], "stateMutability": "view", "type": "function" }, { "inputs": [], "name": "renounceOwnership", "outputs": [], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [{ "internalType": "address[]", "name": "_recipients", "type": "address[]" }, { "internalType": "uint256[]", "name": "_claimableAmount", "type": "uint256[]" }], "name": "setRecipients", "outputs": [], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [{ "internalType": "address payable", "name": "_sweepReceiver", "type": "address" }], "name": "setSweepReciever", "outputs": [], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [], "name": "sweep", "outputs": [], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [], "name": "sweepReceiver", "outputs": [{ "internalType": "address payable", "name": "", "type": "address" }], "stateMutability": "view", "type": "function" }, { "inputs": [], "name": "token", "outputs": [{ "internalType": "contract IERC20VotesUpgradeable", "name": "", "type": "address" }], "stateMutability": "view", "type": "function" }, { "inputs": [], "name": "totalClaimable", "outputs": [{ "internalType": "uint256", "name": "", "type": "uint256" }], "stateMutability": "view", "type": "function" }, { "inputs": [{ "internalType": "address", "name": "newOwner", "type": "address" }], "name": "transferOwnership", "outputs": [], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [{ "internalType": "uint256", "name": "amount", "type": "uint256" }], "name": "withdraw", "outputs": [], "stateMutability": "nonpayable", "type": "function" }], '0x67a24ce4321ab3af51c2d0a4801c3e111d88c9d9');
@@ -116,6 +120,7 @@ claimBtn.onclick = () => {
             claimToken(account).then((receipt) => {
               editRow(index + 1, [account.address, web3.utils.fromWei(balance), "ARB", `<p class="status claimed">CLAIMED</p>`], listWallet);
             }).catch((error) => {
+              console.error(error.message);
               editRow(index + 1, [account.address, web3.utils.fromWei(balance), "ARB", `<p class="status new">NEW</p>`], listWallet);
             }).finally(() => {
               completedCount++;
@@ -137,7 +142,7 @@ claimBtn.onclick = () => {
       if (completedCount == data.length) {
         claimBtn.classList.remove('btn-disabled');
         loading.classList.toggle('active');
-        alert("Done!!!");
+        // alert("Done!!!");
       }
     }
   }
@@ -262,7 +267,7 @@ async function claimToken(accountFrom) {
       data: claimTx.encodeABI(),
       gas: await claimTx.estimateGas({
         from: accountFrom.address,
-        gasPrice: "0x3b9aca00"
+        //gasPrice: "0x3b9aca00"
       }),
     },
     accountFrom.privateKey
